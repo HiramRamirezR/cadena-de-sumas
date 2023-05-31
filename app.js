a33e277b4034705d12973ae492896c1b9eec1081
hiramramirez <hiram.j.ramirez@gmail.com>
2023-05-31 15:54:36 -0500

    changing style

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let randomArray = [];
 let numeros = document.getElementById("numeros");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
@@ -10,21 +9,22 @@ function getRandomNumbers() {
   for (let i = 0; i < 10; i++) {
       randomArray.push(Math.floor(Math.random() * 10));
   }
+  numeros.innerHTML = randomArray;
   return randomArray;
 }
 
-// Create a function that will show one by one the numbers of the array with two seconds delay
-function showNumbers() {
-  let i = 0;
-  let interval = setInterval(function () {
-      if (i < randomArray.length) {
-          numeros.innerHTML = randomArray[i];
-          i++;
-      } else {
-          clearInterval(interval);
-      }
-  }, 8000);
-}
+// Create a function that will show one by one the numbers of the array with n seconds delay
+// function showNumbers() {
+//   let i = 0;
+//   let interval = setInterval(function () {
+//       if (i < randomArray.length) {
+//           numeros.innerHTML = randomArray[i];
+//           i++;
+//       } else {
+//           clearInterval(interval);
+//       }
+//   }, 5000);
+// }
 
 // Create a function to show the sum of the numbers
 function sumNumbers() {
@@ -36,11 +36,11 @@ function sumNumbers() {
 }
 
 button.addEventListener("click", () => {
-  sumNumbers() === parseInt(input.value) ? resultadosActualizados.innerHTML = `${randomArray} = ${sumNumbers()} ✅` : resultadosActualizados.innerHTML = `${randomArray} = ${sumNumbers()} ❌ Tu respuesta: ${input.value}`;
+  sumNumbers() === parseInt(input.value) ? resultadosActualizados.innerHTML = `${randomArray} = ${sumNumbers()} ✅` : resultadosActualizados.innerHTML = `Tu respuesta: ${input.value} ❌ ${randomArray} = ${sumNumbers()}`;
 })
 
 getRandomNumbers();
-showNumbers();
+// showNumbers();
 
 console.log(getRandomNumbers());
 console.log(sumNumbers());
